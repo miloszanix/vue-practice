b7f28adde1fd943b6bee535bdb846a407f08f2fb
Milos Ursulovic <milos.zanix@gmail.com>
2023-05-29 19:01:09 +0200

    Introduce modals

--- src/Components/Modal.vue
+++ src/Components/Modal.vue
@@ -1,28 +1,12 @@
 <script lang="ts">
 
 import { defineComponent } from 'vue';
-import type { ModalProps } from '@/Helpers/Modal';
 
 export default defineComponent({
-  props: {
-    header: {
-      type: String,
-      required: true
-    },
-    text: {
-      type: String,
-      required: true
-    }
-  },
   methods: {
     emitCloseModal(): void { 
       this.$emit('closeModal');
     }
-  },
-  setup(props: ModalProps) {
-    const { header, text } = props;
-
-    return { header, text };
   }
 });
 
@@ -33,8 +17,11 @@ export default defineComponent({
   <div class="fixed inset-0 flex items-center justify-center z-50">
     <div class="bg-gray-800 opacity-75 fixed inset-0" @click="emitCloseModal"></div>
     <div class="bg-white p-6 w-[500px] h-[300px] rounded-lg relative">
-      <h2 class="text-2xl mb-4">{{ header }}</h2>
-      <p>{{ text }}</p>
+      <slot />
+      <slot name="content"/>
+      <p>divider</p>
+      <slot name="links" />
+      <br>
       <button
         class="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
       >
--- src/Pages/Giveaway.vue
+++ src/Pages/Giveaway.vue
@@ -11,6 +11,18 @@ const toggleModal = (): boolean => showModal.value = !showModal.value;
 </script>
 
 <template>
-  <Modal v-if="showModal" @closeModal="toggleModal" header="Test header" text="Text text" />
+  <Modal v-if="showModal" @closeModal="toggleModal">
+    <p>default slot</p>
+    <p>one more</p>
+    <template v-slot:content>
+      <h1>Hey</h1>
+      <p>this is test</p>
+    </template>
+
+    <template v-slot:links>
+      <a href="">Link 1</a><br>
+      <a href="">link 2</a>
+    </template>
+  </Modal>
   <button @click="toggleModal()">Toggle modal</button>
 </template>
