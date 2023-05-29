e22e2734481a68ca66f3bc07c10895dc313671a6
Milos Ursulovic <milos.zanix@gmail.com>
2023-05-29 11:40:15 +0200

    Use vue ref

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+
 const title: string = "This is title";
 const author: string = "Milos Ursulovic";
 const age: number = 27;
+
+let showBooks: Ref<boolean> = ref(false);
+
+const toggleShowBooks = () => showBooks.value = !showBooks.value;
+
 </script>
 
 <template>
-  <h1 className="text-3xl font-bold underline">
+  <h1 v-if="showBooks" className="text-3xl font-bold underline">
     {{ title }} - {{ author }} - {{ age }}
   </h1>
+  <button @click="toggleShowBooks">
+    <span v-if="showBooks">Hide books</span>
+    <span v-else>Show books</span>
+  </button>
 </template>
